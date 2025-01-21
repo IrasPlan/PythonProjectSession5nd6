17d12db17de0e41e715fcd2e4b24262c2621ef6f
IrasPlan <ignairastorza@gmail.com>
2025-01-21 16:55:18 +0100

    Added more print examples

--- input_example.py
+++ input_example.py
@@ -1,4 +1,10 @@
 name = input("What is your name? ")
 print("Hello", name)
+age = input("How old are you ")
+print(name, ", you were born in", 2024-int(age)) #int is so age can turn into a number
+#another way to format print is via f-string
+print(f"{name}, you were born in {2024-int(age)}") #the things that you have giving it earlier that is using has to go on {}, when using f""
+
+
 
 
